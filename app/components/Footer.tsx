4d5a5d4d222c4a0400b6cfb209748ad5d1ddab35
Ash <ashwilliams@domain.tld>
2025-07-17 20:48:43 -0400

    update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,10 +2,7 @@ export default function Footer() {
   return (
     <div className="w-full bg-gray-100 p-2 text-center mt-auto">
       <span className="text-sm">
-        The source code for this site can be found on{" "}
-        <a className="font-semibold hover:underline" href="" target="_blank">
-          Github
-        </a>
+        Crafted with ♡ in Venezuela
       </span>
     </div>
   );
